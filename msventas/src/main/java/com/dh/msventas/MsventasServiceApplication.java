79f667e0b623267f16b7ecc1e27f7556b842ba75
Hector Javier Moreno <cytelsystem@gmail.com>
2023-05-31 09:41:39 -0500

    gateway funcionando

---
+++ msventas/src/main/java/com/dh/msventas/MsventasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dh.msventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsventasServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsventasServiceApplication.class, args);
+  }
+
+}
